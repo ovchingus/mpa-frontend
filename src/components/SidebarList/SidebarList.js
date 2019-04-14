d1a4cbabff228dcb034e90b10247c64f8f199078
George Berezhnoy <gohabereg@users.noreply.github.com>
2019-04-14 13:16:28 +0300

    feat(components): add patients list (#17)
    META: closes #5

--- src/components/SidebarList/SidebarList.js
+++ src/components/SidebarList/SidebarList.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Sidebar, Menu, Ref } from 'semantic-ui-react';
 import NewPatientForm from '../NewPatientFormModal/NewPatientFormModal';
 
+const patients = [
+    { name: 'Василий Петров', gender: 'male', birthdate: '01.01.1970' },
+    { name: 'Петр Васечкин', gender: 'male', birthdate: '01.01.1970' }
+];
+
 export default class SidebarList extends React.Component {
     state = {
         opened: false,
@@ -53,6 +58,14 @@ export default class SidebarList extends React.Component {
                     onHide={() => this.setState({ animating: false })}
                     width='wide'
                 >
+                    <h2>Patients</h2>
+                    {patients.map(patient => {
+                        return (
+                            <Menu.Item as='div'>
+                                {patient.name} ({patient.birthdate})
+                            </Menu.Item>
+                        );
+                    })}
                     <Menu.Item as={NewPatientForm} />
                 </Sidebar>
             </Ref>
