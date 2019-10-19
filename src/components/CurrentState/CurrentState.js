d502ff96a2e62e7d63ed5274f3da29715c2ea33f
mariashipkova <mariashipkova@yandex-team.ru>
2019-10-19 18:31:55 +0300

    Добавлен компонент CurrentState

---
+++ src/components/CurrentState/CurrentState.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class CurrentStateContainer extends React.PureComponent {
+    render () {
+        const { status } = this.props.patient;
+        const { state } = status;
+        return (
+            <section className="CurrentState">
+                <h2>Текущее состояние</h2>
+                <h3>{state.name}</h3>
+                <div>
+                    <h4>Описание</h4>
+                    <p>{state.description}</p>
+                </div>
+                <div>
+                    <h4>Анализы</h4>
+                    <ul>
+                        {status.attributes.length ? status.attributes.map(attr =>
+                            <li key={attr.name}>{JSON.stringify(attr)}</li>)
+                            : 'ничего не назначено'}
+                    </ul>
+                </div>
+                <div>
+                    <h4>Лекарства</h4>
+                    <ul>
+                        {status.medicines.length ? status.medicines.map(attr =>
+                            <li key={attr.name}>{JSON.stringify(attr)}</li>)
+                            : 'ничего не прописано'}
+                    </ul>
+                </div>
+            </section>);
+    }
+};
+
+export const CurrentState = connect(
+    store => ({
+        patient: store.patient
+    })
+)(CurrentStateContainer);
